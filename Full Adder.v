5547dba4df90dded738adf6c9ee71a6dd7a528ff
AZINIZD <93920464+AZINIZD@users.noreply.github.com>
2024-08-04 11:18:05 +0330

    Add files via upload

---
+++ Full Adder.v
@@ -0,0 +1,15 @@
+//***************FullAdder****************//
+module FullAdder(a,b,cin,cout,sum);
+   
+    input a,b,cin; 
+    output sum, cout; 
+    reg sum, cout; // registers retain value
+    
+	always @ (a or b or cin) // Anytime a or b or cin CHANGE, run the 
+   
+   process 
+    begin sum <= a ^ b ^ cin; 
+    cout <= (a & b) | (a & cin) | (b & cin); 
+    end 
+
+endmodule   
